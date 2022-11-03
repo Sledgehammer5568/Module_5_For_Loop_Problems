bf4c2568845e1db125ef43c5d7a90cf6bd1dfdce
Emanuel Ramos <115571450+Sledgehammer5568@users.noreply.github.com>
2022-11-02 19:10:45 -0500

    FourForLoopProblems

--- ForLoopProblemFour.py
+++ ForLoopProblemFour.py
@@ -3,8 +3,8 @@
 # 11/1/2022
 #
 # Description: prints a list of numbers from 1 to 50 and prints Divisible by 3 for all numbers
-# divisible by 3 as well as printing Divisible by 5 for any number divisible by 5.
-#
+# divisible by 3 as well as printing Divisible by 5 for any number divisible by 5
+# also prints out Divisible by both for numbers divisible by both 3 and 5.
 
 for numbers in range(1, 51):
     if numbers % 3 == 0 and numbers % 5 == 0:
